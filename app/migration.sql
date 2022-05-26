cd25a6e4370d6a240e1ea56e4ea38dedd7303ef0
Lucas Perez <pluqui1@gmail.com>
2022-05-26 13:51:47 +0200

    fix migrations file
    remove bigint declaration for id
    fix first_name field

--- app/migration.sql
+++ app/migration.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE users_db;
 
 CREATE TABLE users_db.users (
-    id           BIGINT(20) NOT NULL AUTO_INCREMENT,
-    fist_name    VARCHAR(45) NOT NULL,
+    id           INT(20) NOT NULL AUTO_INCREMENT,
+    first_name   VARCHAR(45) NOT NULL,
     last_name    VARCHAR(45) NOT NULL,
     email 		 VARCHAR(45) NOT NULL,
     date_created VARCHAR(45) NOT NULL,
